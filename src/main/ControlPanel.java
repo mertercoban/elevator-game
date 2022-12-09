c9b41a2a2aa42ef834addfed96a7522047f45133
sekerhastasi <sekerhastasi00@gmail.com>
2022-12-09 21:48:10 +0300

    .

--- src/main/ControlPanel.java
+++ src/main/ControlPanel.java
@@ -1,7 +1,5 @@
 package main;
 
-import javafx.event.Event;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
@@ -15,6 +13,7 @@ public class ControlPanel extends GridPane {
     private Button downButton;
     private int upButtonClickCount = 0;
     private int downButtonClickCount = 0;
+
     public ControlPanel(ElevatorGame game) {
         this.game = game;
         setPrefWidth(TILE_SIZE * 10);
@@ -29,30 +28,30 @@ public class ControlPanel extends GridPane {
         downButton.setDisable(true);
 
         upButton.setOnAction(actionEvent -> {
-                upButtonClickCount++;
-                System.out.println("Up : "+upButtonClickCount);
-                game.goUp();
-                upButton.setDisable(true);
-                downButton.setDisable(true);
+            upButtonClickCount++;
+            System.out.println("Up : " + upButtonClickCount);
+            game.goUp();
+            upButton.setDisable(true);
+            downButton.setDisable(true);
         });
 
         downButton.setOnAction(actionEvent -> {
-                downButtonClickCount++;
-                System.out.println("Down : "+downButtonClickCount);
-                game.goDown();
-                upButton.setDisable(true);
-                downButton.setDisable(true);
+            downButtonClickCount++;
+            System.out.println("Down : " + downButtonClickCount);
+            game.goDown();
+            upButton.setDisable(true);
+            downButton.setDisable(true);
         });
 
         getChildren().add(upButton);
         getChildren().add(downButton);
 
-        upButton.setStyle("-fx-background-color: #1fabab; -fx-border-color: black;-fx-font-family: Harrington; " +
+        upButton.setStyle("-fx-background-color: #1fabab; -fx-border-color: black;-fx-font-family: 'Arial Black'; " +
                 "-fx-font-size: 25px; -fx-border-width: 3px; -fx-border-radius: 7px; -fx-background-radius: 10px; " +
-                "-fx-padding: 17px;-fx-translate-x: 100px;-fx-max-height: 65px;-fx-min-width: 110px");
-        downButton.setStyle("-fx-background-color: #cb701d; -fx-border-color: black; -fx-font-family: Harrington; " +
-                "-fx-font-size: 25px; -fx-border-width: 3px; -fx-border-radius: 7px; -fx-background-radius: 10px;" +
-                " -fx-translate-y: 90px; -fx-translate-x: 100px; -fx-padding:16px;-fx-max-height: 65px;-fx-min-width: 110px");
+                "-fx-padding: 17px;-fx-translate-y: 30px;-fx-translate-x: 100px;-fx-max-height: 65px;-fx-max-width: 130px");
+        downButton.setStyle("-fx-background-color: #cb701d; -fx-border-color: black; -fx-font-family: 'Arial Black'; " +
+                "-fx-font-size: 25px; -fx-border-width: 3px;-fx-border-radius: 7px; -fx-background-radius: 10px;" +
+                " -fx-translate-y: 120px; -fx-translate-x: 100px; -fx-padding:16px;-fx-max-height: 65px;-fx-max-width: 130px");
 
     }
 
@@ -62,7 +61,7 @@ public class ControlPanel extends GridPane {
                 upButton.setDisable(false);
                 downButton.setDisable(true);
             }
-            case 1,2 -> {
+            case 1, 2 -> {
                 upButton.setDisable(false);
                 downButton.setDisable(false);
             }
